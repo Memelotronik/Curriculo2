b237f26ca566ff2f510b57830b251ac1bf621672
Memelotronik <memelotronik@gmail.com>
2020-02-19 22:59:51 -0600

    Primera version

---
+++ Inicio.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Curriculo</title>
+	<style type="text/css">
+		@keyframes animacion_titulo{
+			from{
+				background-color: #05FCF8;
+			}
+			to{
+				background-color: #00FD04;
+			}
+		}
+		.titulo{
+			width: 100%;
+			background-color: #FFFFFF;
+			color: white;
+			text-align: center;
+			height: 150px;
+			display: flex;
+			justify-content: center;
+			animation-name: animacion_titulo;
+			animation-duration: 2s;
+			animation-direction: alternate;
+			animation-iteration-count: infinite;
+		}
+	</style>
+</head>
+<body>
+	<div class="titulo">
+		<CENTER><H1><B>Reyes Vega Angel Leonardo</B></H1></CENTER>
+	</div>
+	<div class="Foto">
+		<CENTER><img src="foto.jpg" alt="Foto personal"></CENTER>
+	</div>
+	<HR>
+	<div class="Acercade">
+		<H2>Acerca de</H2>
+		Estudiante en Nivel Medio Superior en el Centro de Estudios Cientificos y Tecnologicos Numero<BR>
+		9 "Juan de Dios Batiz Paredes" actualmente cursando la carrera tecnica de Programación<BR>
+		misma de la cual ha cursado en su tercer semestre las materias de Programación Orientada a<BR>
+		Objetos (POO), Laboratorio de Proyectos de Tecnologias de la Información I y Administración<BR>
+		de Proyectos de Tecnologias de la Información I. Actualmente cursando cuarto semestre con<BR>
+		las materias de Programación y Servicios Web (PSW), Bases de Datos, Laboratorio de Proyectos<BR>
+		de la Información II y Tecnicas de Programación Personal con Calidad (TPPC).
+	</div>
+	<HR>
+	<div class="Experiencia">
+		<H2>Experiencia</H2>
+		<UL>
+			<LI>
+				Computación Basica I (Microsoft Word y Microsoft Power Point).
+			</LI>
+			<LI>
+				Computación Basica II (Microsoft Excel y HTML).
+			</LI>
+			<LI>
+				Programación Orientada a Objetos (POO).
+			</LI>
+			<LI>
+				Laboratorio de Proyectos de Tecnologias de la Información I.
+			</LI>
+			<LI>
+				Administración de Proyectos de Tecnologias de la Información I.
+			</LI>
+		</UL>
+	</div>
+	<HR>
+	<div class="Educacion">
+		<H2>Educacion</H2>
+		<a href="https://www.institutocrisol.edu.mx/"><img src="crisol.jpg" align="left" height="100" width="100" alt="Instituto Crisol"></a>
+		Instituto Crisol<BR>
+		2014-2017<BR>
+		Escuela Nivel Preescolar, Primaria y Secundaria<BR>
+		Escuela Catolica<BR>
+		Promedio de 8.3<BR>
+		<BR>
+		<BR>
+		<BR>
+		<a href="https://www.cecyt9.ipn.mx/"><img src="cecyt.jpg" align="left" height="100" width="100" alt="CECYT 9"></a>
+		Centro de Estudios Cientificos y Tecnologicos Numero 9 "Juan de Dios Batiz Paredes"<BR>
+		2017-Actualidad<BR>
+		Escuela Nivel Medio Superior<BR>
+		Promedio actual de 7.96<BR>
+		<BR>
+		<BR>
+		<BR>
+		<BR>
+	</div>
+	<HR>
+	<div class="Idiomas">
+		<H2>Idiomas</H2>
+		<B>Español</B><BR>
+		Lengua Nativa<BR>
+		<BR>
+		<B>Ingles</B><BR>
+		Lengua Secundaria<BR>
+		<BR>
+	</div>
+</body>
+</html>
